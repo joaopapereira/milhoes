49ebe6cb23043df7ee00759dbdeb0c2d6e8d8126
João Pereira <joaopapereira@gmail.com>
2015-03-31 15:19:03 +0000

    Added VCR gem Added tests for Feedjira

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Feed, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    before(:all) do
+      VCR.use_cassette('feeds') do
+        Feed.read_feed
+      end
+    end
+  describe "feed#" do
+    it "one feed" do
+        expect(Feed.all.size).to eq(1)
+    end
+    it "Correct values" do
+        expect(Feed.all.first.as_jackpot).to eq(true)
+        expect(Feed.all.first.prize).to eq(73000000)
+        expect(Feed.all.first.last_key).to eq(" 2 30 32 39 44 +  6 10")
+        expect(Feed.all.first.next_game_date).to eq("31/03/2015".to_date)
+    end
+  end
 end
